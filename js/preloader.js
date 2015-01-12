4217a453f4eef2d70951dbebb3e5eaba2ed1149e
DanHacker <dan.hacker@twofour.co.uk>
2015-01-12 09:37:22 +0000

    first commit

---
+++ js/preloader.js
@@ -0,0 +1,65 @@
+/*
+	Publish:
+		navigate
+
+	Subscribe:
+	
+*/
+
+//define([
+//	'jquery'	
+//], function($){
+
+
+	
+	
+		(function(){
+		
+			function preloader(){
+				
+				var dfdInitialPreload = $.Deferred();
+				
+				function _init(){console.log('preloader.init');}
+				
+				function _preloadImages(imgArr){
+					console.log('preloader.preloadImages', imgArr);
+					imgArr == typeof(imgArr) == "undefined" ? [] : imgArr;
+					var dfd = $.Deferred(),
+						count  = 0,
+						imagesToLoad = imgArr.length,
+						images = [];
+					
+					$.each(imgArr, function(i, o){
+						images[i] = new Image();
+						$(images[i]).load(function(){
+							if (--imagesToLoad == 0){
+								dfdInitialPreload.resolve();
+								dfd.resolve();
+							} else {
+								var newImg = $('<img/>').attr('src', $(this).attr('src'));
+								
+								count++;
+								dfd.notify(count, o);
+								
+							}
+						}).attr('src', imgArr[i]);
+					
+					});
+					return dfd.promise();
+				}
+					
+				_init();
+				
+				return {
+					preloadImages : _preloadImages
+				}
+			
+			
+		}
+		
+		console.log('preloader.self-executing preloader');
+		window.preloader = preloader();
+			
+	})();
+	
+//});
